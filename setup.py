b7f134828ec7d7e038fb625daa36755773bf3107
Michael Mensah Ofeor <michaelofeor2011@yahoo.com>
2024-11-19 12:51:00 +0000

    Initial commit for Bank Churn Prediction project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Businesses like banks which provide service have to worry about problem of 'Customer Churn' i.e. customers leaving and joining another service provider. It is important to',
+    author='Michael Mensah Ofeor',
+    license='MIT',
+)
